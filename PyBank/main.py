f2c5b9c44f9fea714572b2e595beb9f9097e2c63
Kasey Geist <kaseygeist@gmail.com>
2019-09-04 17:07:53 -0700

    moving stuff because csv won't read

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,9 +2,9 @@
 import os
 import csv
 
+PyBank_csv = os.path.join('budget_data.csv')
 
-PyBank_csv = os.path.join("Resources","budget_data.csv")
-
+print(PyBank_csv)
 with open(PyBank_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
